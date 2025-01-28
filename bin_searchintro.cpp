0cc6b8b443c3247d56ce8f3314884c263cca3722
parthgiramkar <imparthgiramkar@gmail.com>
2025-01-28 20:04:44 +0530

    Update bin_searchintro.cpp

--- bin_searchintro.cpp
+++ bin_searchintro.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
+#include<queue>
 
-class node {
+class binarysearch {
 public:
     int data ;
-    node *left;
-    node *right;
+    binarysearch *left; 
+    binarysearch *right;
     
-    node(int d) {
+    binarysearch(int d) {
         this->data = d;
         this->right = right;
         this->left = left;
     }
 };
 //from scratch i.e from the root
-node *buildtree(node *root) {
-        cout<<"enter the data  : ";
+binarysearch *buildtree(binarysearch *root) {
+        cout<<"enter the data  : "<<endl;
         int data;
         cin>>data;
-        root = new node(data);
+        root = new binarysearch(data);
+        
         if(data == -1) {
             return NULL;
         }
@@ -26,16 +28,44 @@ node *buildtree(node *root) {
         root->left = buildtree(root->left) ; 
         
         cout<<"enter the data for inserting in right of "<<data<<" - "<<endl;
-        root->right = buildtree(root->left) ;
+        root->right = buildtree(root->right) ;
+        
         return root;
 }
 //levelwise traversal from left->right
-node *
+void levelwisetraversal(binarysearch *root) {
+    queue<binarysearch *>q;    //FIFO order 
+    q.push(root);
+    q.push(NULL);
+    
+    while(!q.empty()) {
+        binarysearch *temp = q.front();
+        q.pop();
+        if(temp==NULL) {  //means the whole level has been traversed
+            cout<<endl; //shift to new line
+            
+            if(!q.empty()){    //has some children left means at the end null of parent level
+                q.push(NULL);
+            }
+        }else {
+            cout << temp -> data;
+            if(temp ->left) {
+                q.push(temp ->left);
+            }
+            if(temp ->right) {
+                q.push(temp ->right);
+            }
+        }
+    }
+    
+}
 
 int main() {
-    node *root = NULL;
+    binarysearch *root = NULL;
     //create a node
     root = buildtree(root);
-    cout<<" ";
+    
+    cout<<"Printing the level wise traversal output - "<<endl;
+    levelwisetraversal(root); 
     
 }
