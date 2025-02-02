16ab71ec7cff596619fcd5376545f3cdafff08f2
parthgiramkar <imparthgiramkar@gmail.com>
2025-02-02 16:56:17 +0530

    Update binary_treeintro.cpp

--- binary_treeintro.cpp
+++ binary_treeintro.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 #include<queue>
 
-class binarysearch {
+class binarytree {
 public:
     int data ;
-    binarysearch *left; 
-    binarysearch *right;
+    binarytree *left; 
+    binarytree *right;
     
-    binarysearch(int d) {
+    binarytree(int d) {
         this->data = d;
         this->right = right;
         this->left = left;
     }
 };
 //from scratch i.e from the root
-binarysearch *buildtree(binarysearch *root) {
+binarytree *buildtree(binarytree *root) {
         cout<<"enter the data  : "<<endl;
         int data;
         cin>>data;
-        root = new binarysearch(data);
+        root = new binarytree(data);
         
         if(data == -1) {
             return NULL;
@@ -32,14 +32,14 @@ binarysearch *buildtree(binarysearch *root) {
         
         return root;
 }
-//levelwise traversal from left->right
-void levelwisetraversal(binarysearch *root) {
-    queue<binarysearch *>q;    //FIFO order 
+//levelwise traversal from left->right using recursion
+void levelwisetraversal(binarytree *root) {
+    queue<binarytree *>q;    //FIFO order 
     q.push(root);
     q.push(NULL);
     
     while(!q.empty()) {
-        binarysearch *temp = q.front();
+        binarytree *temp = q.front();
         q.pop();
         if(temp==NULL) {  //means the whole level has been traversed
             cout<<endl; //shift to new line
@@ -58,33 +58,67 @@ void levelwisetraversal(binarysearch *root) {
         }
     }
 }
-void inordertraversal(binarysearch *root) {
-    if(root==NULL) {
+void inordertraversal(binarytree *root) {
+    if(root==NULL) {   //follows LNR
         return ;
     }
     inordertraversal(root->left);
     cout<<root->data<<" ";
     inordertraversal(root->right);
 }
-void preordertraversal(binarysearch *root) {
-    if(root==NULL){
+void preordertraversal(binarytree *root) {
+    if(root==NULL){           //follows NLR
         return ;
     }
     cout<<root->data<<" ";
     preordertraversal(root->left);
     preordertraversal(root->right);
 }
-void postordertraversal(binarysearch *root) {
-    if(root==NULL){
+void postordertraversal(binarytree *root) {
+    if(root==NULL){    //follows LRN
         return ;
     }
     postordertraversal(root->left);
     postordertraversal(root->right);
     cout<<root->data<<" ";
 }
+/*
+    1 
+   3 5 
+  7 11 17  - into tree without recursion
+*/ 
+void buildlevelordertree(binarytree *&root) {
+    queue<binarytree*>q;
+    cout<<"Enter the data for root node - "<<endl;
+    int data;
+    cin>>data;
+    root = new binarytree(data);
+    q.push(root);
+    while(!q.empty()) {
+        binarytree *temp = q.front();
+        q.pop();
+        cout<<"enter the data for left node of "<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata != -1) {
+            temp->left = new binarytree(leftdata);
+            q.push(temp->left);
+        }
+        cout<<"enter the data for right node of "<<temp->data<<endl;
+        int rightdata;
+        cin>>rightdata;
+        if(rightdata != -1) {
+            temp->right = new binarytree(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
 
 int main() {
-    binarysearch *root = NULL;
+    binarytree *root = NULL;
+    buildlevelordertree(root);
+    
+    /*
     //create a node
     root = buildtree(root);
     
@@ -102,7 +136,6 @@ int main() {
     cout<<"The postorder traversal - "<<endl;
     postordertraversal(root);
     cout<<endl;
+    */
     
 }
-
-
